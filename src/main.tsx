e665bd6d9267a826c3ca57f1f50aba2309211910
joshuatuan <itsjoshuatuan+git@gmail.com>
2024-11-26 21:16:08 +0800

    testing out sass

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { QuizProvider } from "./contexts/QuizContext";
-import "./index.css";
+import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
